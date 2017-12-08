2a02f45055b88c05fa60b5ccd5bc775b707fa16f
theo <brossiertheo@live.fr>
2017-12-08 18:48:26 +0100

    modif

--- js/Snake.js
+++ js/Snake.js
@@ -7,15 +7,32 @@ export default class Snake {
         this.vel = new Vector2()
         this.color = color
         this.tailLength = 3
+        this.tail = []
 
     }
     draw () {
         this.ctx.fillStyle = this.color
         this.ctx.fillRect(this.pos.x, this.pos.y, this.cell, this.cell)
+        this.tail.forEach(t => {
+                this.ctx.fillStyle = this.color
+                this.ctx.fillRect(t.x, t.y, this.cell, this.cell)
+        })
+        while(this.tail.length < this.tailLength){
+            this.tail.push({x:this.pos.x,y: this.pos.y})
+        }
+        console.log(this.pos.x, this.pos.y)
+        console.table(this.tail)
+        this.tail.shift()
+
     }
     update () {
         this.pos.move(this.pos.x+this.vel.x*this.cell,
                     this.pos.y + this.vel.y * this.cell)
-
+        
+        if(this.pos.x < 0) this.pos.x = this.cell*this.cell
+        if(this.pos.x > this.cell*this.cell) this.pos.x = 0
+        if(this.pos.y < 0) this.pos.y = this.cell*this.cell
+        if(this.pos.y > this.cell*this.cell) this.pos.y = 0
+        
     }
 }
